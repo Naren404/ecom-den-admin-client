1a74ef6a4f01f4002b8cd40626a0d43f2ce1f342
Naren404 <narendrasunar47@gmail.com>
2024-03-03 09:25:55 +1100

    signup feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Route, Routes } from "react-router-dom"
+import SignupPage from "./pages/authPage/signupPage"
+import { ToastContainer } from 'react-toastify';
+import VerifyEmailPage from './pages/authPage/verifyEmailPage';
+
 function App() {
 
   return (
     <>
-      <h1>Ecom-Den-Admin</h1>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/signup" element={<SignupPage /> } />
+        <Route path="/verify-email" element={<VerifyEmailPage /> } />
+      </Routes>
+
+      <ToastContainer />
     </>
   )
 }
