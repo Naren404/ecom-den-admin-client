2f6960b3d022baf907f976ee27b2faeeaac8f7f0
Naren404 <narendrasunar47@gmail.com>
2024-03-03 13:12:48 +1100

    admin-layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import SignupPage from "./pages/authPage/signupPage"
 import { ToastContainer } from 'react-toastify';
 import VerifyEmailPage from './pages/authPage/verifyEmailPage';
 import LoginPage from './pages/authPage/loginPage';
+import AdminPrivateRoutes from './components/privateRouter/adminPrivateRouter';
+import AdminLayout from './components/layouts/adminLayout';
 
 function App() {
 
@@ -16,8 +18,17 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage /> } />
         <Route path="/verify-email" element={<VerifyEmailPage /> } />
-      </Routes>
 
+      {/* Private Routes */}
+      <Route path='/admin' element={<AdminPrivateRoutes><AdminLayout /></AdminPrivateRoutes>}>
+        <Route path="" element={<p>DashBoard</p>} />
+        <Route path="categories" element={<p>Category Page</p>} />
+        <Route path="products" element={<p>Products</p>} />
+        <Route path="orders" element={<p>Orders</p>} />
+        <Route path="users" element={<p>Users</p>} />
+      </Route>
+
+    </Routes>
       <ToastContainer />
     </>
   )
