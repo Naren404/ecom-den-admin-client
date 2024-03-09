ea5f25843c4f6fa9348fe0659d2a85ccb526b8ef
Naren404 <narendrasunar47@gmail.com>
2024-03-09 13:53:50 +1100

    creaete-product-feature

--- src/entities/product/productActions.js
+++ src/entities/product/productActions.js
@@ -1,6 +1,7 @@
 import { toast } from "react-toastify"
 import { setProducts } from "./productSlice"
-import { getProducts } from "./productAxiosHelper"
+import { createProduct, getProducts, updateProduct } from "./productAxiosHelper"
+import { setIsCreating } from "../../utility/promiseSlice"
 
 // GET ALL PRODUCTS
 export const getProductsAction = () => async(dispatch) => {
@@ -11,4 +12,40 @@ export const getProductsAction = () => async(dispatch) => {
   }
 
   dispatch(setProducts(result.data))
+}
+
+// CREATE A PRODUCT
+export const createProductAction = (productObj) => async(dispatch) => {
+
+  //set isCreating true
+  dispatch(setIsCreating(true))
+  // call create category API
+  const result = await createProduct(productObj)
+  // set isCreating false
+  dispatch(setIsCreating(false))
+
+  if(result?.status === "error"){
+    return toast.error(result.message)
+  }
+
+  toast.success(result.message)
+  dispatch(getProductsAction())
+}
+
+// UPDATE A CATEGORY
+export const updateProductAction = (productObj) => async(dispatch) => {
+
+  //set isCreating true
+  dispatch(setIsCreating(true))
+  // call create category API
+  const result = await updateProduct(productObj)
+  // set isCreating false
+  dispatch(setIsCreating(false))
+
+  if(result?.status === "error"){
+    return toast.error(result.message)
+  }
+
+  toast.success(result.message)
+  dispatch(getProductsAction())
 }
---
+++ src/pages/productPage/newProductPage.jsx
@@ -0,0 +1,28 @@
+import { Alert } from "react-bootstrap";
+import ProductForm from "../../components/productForm/productForm";
+
+const initialFormData = {
+  thumbnail: '',
+  parentCategory: '',
+  name: '',
+  price: '',
+  quantity: '',
+  sku: '',
+  description: '',
+  status: '',
+  salesPrice: '',
+  salesStartDate: '',
+  salesEndDate: '',
+}
+
+const NewProductPage = () => {
+  return ( 
+    <>
+      <Alert variant="info" className="text-dark fw-bold fs-4">Add New Product</Alert>
+
+      <ProductForm initialFormData={initialFormData} />
+    </>
+   );
+}
+ 
+export default NewProductPage;
